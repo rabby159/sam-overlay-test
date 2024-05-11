cbf3c156f5d9404c4b9143b0390ac736e5017445
Md Rabby <ablazerabbi159@gmail.com>
2024-05-11 14:51:36 +0600

    responsive issue

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
             </div>
             <div className='absolute mt-12 lg:p-36 lg:mt-40 lg:ml-96  justify-center '>
                 <p className='text-sm lg:text-6xl text-center text-white shadowT'>Hey, I am <span className='text-cyan-400'>Reed!</span></p>
-                <p className='text-sm lg:text-2xl mx-10 text-center mt-5 tracking-widest text-white'>I am a full-time freelance designer from  <br /> I am specialized in VTuber Graphics.</p>
+                <p className='text-sm lg:text-2xl mx-10 text-center mt-5 tracking-widest text-white'>I am a full-time freelance designer from Bangladesh. <br /> I am specialized in VTuber Graphics.</p>
             </div>
         </div>
         </>
