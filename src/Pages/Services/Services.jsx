d3d127657bb9cb9a0783e5d91a1175d7ce4baa3c
Md Rabby <ablazerabbi159@gmail.com>
2024-06-07 23:47:25 +0600

    update

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
         <div className="bg-black">
             <div className="">
                 <h2>Services</h2>
-                <p>Please note that final prices are base price plus 6% VAT.</p>
+                <p>Please note that final prices are base price plus 7% VAT</p>
             </div>
         </div>
     );
